370fcb8fa5d08944d6839949b285a9da44ef7670
aaitken24 <aitken24200@lcsd2kids.org>
2021-05-10 11:44:16 -0600

    I finished the project. I added buttons to change the color, and a spot that tells you if you are drawing or not.
    I added new class modifiers and aligned items.
    
    I added a variable to tell you if you are drawing or not. I also made it so you don't have to draw and you can toggle this with clicking.

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 const gridContainer = document.querySelector("#grid-container");
-let numOfTimes = 256;
-let userColor = prompt("What color do you want?")
+let numOfTimes = 279;
+let userColor = "red";
+let colorMessage = "On"
+let isColorOn = true;
+function changeToRed() {
+    userColor = "red"
+}
+function changeToGreen() {
+    userColor = "green"
+}
+function changeToBlue() {
+    userColor = "blue"
+}
+function changeToYellow() {
+    userColor = "yellow"
+}
+function changeToPink() {
+    userColor = "pink"
+}
+function changeToPurple() {
+    userColor = "purple"
+}
+function changeToWhite() {
+    userColor = "white"
+}
+function changeToBlack() {
+    userColor = "black"
+}
+function turnColorOff() {
+    isColorOn = false
+    colorMessage = " Off"
+}
+function turnColorOn() {
+    isColorOn = true
+    colorMessage = " On"
+}
 for (let i =0; i<numOfTimes; i++) {
     const singleCell = document.createElement("div")
-    singleCell.textContent = "Hello?";
-    singleCell.style.color = "White";
-    singleCell.classList.add('blackBackground', 'alignmentClass')
-    singleCell.addEventListener('click', () => {
+    singleCell.classList.add('blackBackground', 'alignmentClass', 'cell')
+    singleCell.addEventListener('mouseover', () => {
+    if (isColorOn) {
         if (userColor == "green") {
             singleCell.classList.remove('redBackground', 'greenBackground', 'blueBackground', 'purpleBackground', 'yellowBackground', 'pinkBackground', 'whiteBackground', 'blackBackground')
             singleCell.classList.add("greenBackground")
@@ -35,6 +68,65 @@ for (let i =0; i<numOfTimes; i++) {
             singleCell.classList.remove('redBackground', 'greenBackground', 'blueBackground', 'purpleBackground', 'yellowBackground', 'pinkBackground', 'whiteBackground', 'blackBackground')
             singleCell.classList.add("whiteBackground")
         }
+        else if (userColor == "black") {
+            singleCell.classList.remove('redBackground', 'greenBackground', 'blueBackground', 'purpleBackground', 'yellowBackground', 'pinkBackground', 'whiteBackground', 'blackBackground')
+            singleCell.classList.add("blackBackground")
+        }
+    }
     });
+    singleCell.addEventListener('click', () => {
+        if (isColorOn) {
+            turnColorOff();
+            colorMessageFunction();
+        }
+        else if (isColorOn == false) {
+            turnColorOn();
+            colorMessageFunction();
+        }
+    })
     gridContainer.appendChild(singleCell);
 }
+const colorButtonRed = document.querySelector("#redColorButton")
+colorButtonRed.addEventListener('click', () => {
+        changeToRed();
+        console.log(userColor)
+})
+const colorButtonGreen = document.querySelector("#greenColorButton")
+colorButtonGreen.addEventListener('click', () => {
+    changeToGreen();
+    console.log(userColor)
+})
+const colorButtonBlue = document.querySelector("#blueColorButton")
+colorButtonBlue.addEventListener('click', () => {
+    changeToBlue();
+    console.log(userColor)
+})
+const colorButtonYellow = document.querySelector("#yellowColorButton")
+colorButtonYellow.addEventListener('click', () => {
+    changeToYellow();
+    console.log(userColor)
+})
+const colorButtonPink = document.querySelector("#pinkColorButton")
+colorButtonPink.addEventListener('click', () => {
+    changeToPink();
+    console.log(userColor)
+})
+const colorButtonPurple = document.querySelector("#purpleColorButton")
+colorButtonPurple.addEventListener('click', () => {
+    changeToPurple();
+    console.log(userColor)
+})
+const colorButtonWhite = document.querySelector("#whiteColorButton")
+colorButtonWhite.addEventListener('click', () => {
+    changeToWhite();
+    console.log(userColor)
+})
+const colorButtonBlack = document.querySelector("#blackColorButton")
+colorButtonBlack.addEventListener('click', () => {
+    changeToBlack();
+    console.log(userColor)
+})
+function colorMessageFunction (){
+    document.getElementById("colorOnText").innerHTML = colorMessage;
+}
+colorMessageFunction();
